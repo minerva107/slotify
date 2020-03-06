42f5af13acffa5c3857da39936e7551a2a18e0ae
Simone Fernandes <55145901+minerva107@users.noreply.github.com>
2020-03-06 19:12:59 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("localhost", "mercado", "123456", "slotify");
+	$con = mysqli_connect("ftp.gear.host", "simonify", "jEbpvW9mBLMlFQFWRN34k3NmkwMnAJpNEJg1nKQRsFFkdjP3bkhvAoHwrihv", "slotify");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
