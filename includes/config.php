f13c5f08aaccbbed9e156208570f1f9cc8371a0a
Simone Fernandes <55145901+minerva107@users.noreply.github.com>
2020-05-26 16:05:35 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("den1.mysql1.gear.host", "slotify2", "Hm9Q_h1~Jx4w", "slotify2");
+	$con = mysqli_connect("localhost", "mercado", "123456", "slotify");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
